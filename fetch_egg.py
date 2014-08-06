0d9e5bb4dac65969c7fddddff2d38c97435d614f
Matthew Taylor <rhyolight@gmail.com>
2014-08-05 17:01:30 -0700

    Trying to install from binary distro.

--- fetch_egg.py
+++ fetch_egg.py
@@ -1,22 +1,31 @@
 import sys
 import requests
+from boto.s3.connection import S3Connection
 
 REGION = "us-west-2"
 BUCKET = "artifacts.numenta.org"
 REPO = "numenta/nupic"
 SHA_FILE = "nupic_sha.txt"
+LOCAL_BINARY_ARCHIVE = "nupic-archive.tar.gz"
 
+def fetchNupicEggFor(sha):
+  conn = S3Connection()
+  artifactsBucket = conn.get_bucket(BUCKET)
+  s3Url = None
 
+  for key in artifactsBucket.list(prefix="%s/%s" % (REPO, sha)):
+    if key.name[-2:] == "gz":
+      s3Url = "https://s3-%s.amazonaws.com/%s/%s" % (REGION, BUCKET, key.name)
 
-def fetchNupicTarballFor(sha):
-  tarballName = "nupic-archive.tar.gz"
-  s3Url = "https://s3-%s.amazonaws.com/%s/%s/%s/%s" % (REGION, BUCKET, REPO, sha, tarballName)
+  if s3Url is None:
+    raise ValueError("Couldn't find binary archive!")
+
   print "Fetching archive from %s..." % s3Url
   blockCount = 0
-  with open(tarballName, "wb") as handle:
+  with open(LOCAL_BINARY_ARCHIVE, "wb") as handle:
     response = requests.get(s3Url, stream=True)
     if not response.ok:
-      raise Exception("Cannot fetch tarball from %s" % s3Url)
+      raise Exception("Cannot fetch NuPIC egg from %s" % s3Url)
     for block in response.iter_content(1024):
       if blockCount % 100 == 0:
         sys.stdout.write('.')
@@ -26,7 +35,6 @@ def fetchNupicTarballFor(sha):
         break
       handle.write(block)
     print "\nDone."
-  return tarballName
 
 
 
@@ -38,4 +46,4 @@ def getSha():
 
 if __name__ == "__main__":
   sha = getSha()
-  fetchNupicTarballFor(sha)
+  fetchNupicEggFor(sha)
