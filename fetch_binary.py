8ccb7eb758378d4e13d85242e6976b3dd5659d48
Matthew Taylor <matt@numenta.org>
2014-12-15 23:34:59 -0800

    Changed name of wheel storage directory
    to "wheelhouse"

--- fetch_binary.py
+++ fetch_binary.py
@@ -8,7 +8,7 @@ REGION = "us-west-2"
 BUCKET = "artifacts.numenta.org"
 REPO = "numenta/nupic"
 SHA_FILE = "nupic_sha.txt"
-WHEEL_DIR = "wheel"
+WHEEL_DIR = "wheelhouse"
 AWS_KEY = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET = os.environ["AWS_SECRET_ACCESS_KEY"]
 
