55ee642e8e4ed077e45931fe4065a523e8553e3d
Matthew Taylor <matt@numenta.org>
2014-12-15 23:19:28 -0800

    Updating to use new release format (wheels)
    Fetch script now looks for .whl files, downloads them into a "wheels"
    directory.

--- fetch_binary.py
+++ fetch_binary.py
@@ -2,33 +2,29 @@ import sys
 import os
 import requests
 from boto.s3.connection import S3Connection
+import shutil
 
 REGION = "us-west-2"
 BUCKET = "artifacts.numenta.org"
 REPO = "numenta/nupic"
 SHA_FILE = "nupic_sha.txt"
-LOCAL_BINARY_ARCHIVE = "nupic-archive.tar.gz"
+WHEEL_DIR = "wheel"
 AWS_KEY = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET = os.environ["AWS_SECRET_ACCESS_KEY"]
 
-def fetchNupicEggFor(sha):
-  conn = S3Connection(AWS_KEY, AWS_SECRET)
-  artifactsBucket = conn.get_bucket(BUCKET)
-  s3Url = None
 
-  for key in artifactsBucket.list(prefix="%s/%s" % (REPO, sha)):
-    if key.name[-2:] == "gz":
-      s3Url = "https://s3-%s.amazonaws.com/%s/%s" % (REGION, BUCKET, key.name)
 
-  if s3Url is None:
-    raise ValueError("Couldn't find binary archive!")
+def getScriptPath():
+  return os.path.dirname(os.path.realpath(sys.argv[0]))
 
-  print "Fetching archive from %s..." % s3Url
+
+
+def fetchWheel(url, localFilePath):
   blockCount = 0
-  with open(LOCAL_BINARY_ARCHIVE, "wb") as handle:
-    response = requests.get(s3Url, stream=True)
+  with open(localFilePath, "wb") as handle:
+    response = requests.get(url, stream=True)
     if not response.ok:
-      raise Exception("Cannot fetch NuPIC egg from %s" % s3Url)
+      raise Exception("Cannot fetch wheel from %s" % url)
     for block in response.iter_content(1024):
       if blockCount % 100 == 0:
         sys.stdout.write('.')
@@ -41,6 +37,37 @@ def fetchNupicEggFor(sha):
 
 
 
+def fetchWheels(sha):
+  conn = S3Connection(AWS_KEY, AWS_SECRET)
+  artifactsBucket = conn.get_bucket(BUCKET)
+  wheelDir = os.path.join(getScriptPath(), WHEEL_DIR)
+  url = None
+
+  # Here's where we put the wheels!
+  if not os.path.exists(wheelDir):
+    os.makedirs(wheelDir)
+
+  for key in artifactsBucket.list(prefix="%s/%s" % (REPO, sha)):
+    # Fail fast.
+    if not key.name.endswith(".whl"):
+      raise ValueError("Expected .whl file, found %s" % key.name)
+
+    # Expects a key like:
+    # numenta/nupic/9e17ca7caef03a2b1aa925e010ee76c1a74ec3cc/setuptools-8.0.4-py2.py3-none-any.whl
+    print key.name
+    wheelName = key.name.split("/").pop()
+
+    url = "https://s3-%s.amazonaws.com/%s/%s" % (REGION, BUCKET, key.name)
+    # Looks like:
+    # https://s3-us-west-2.amazonaws.com/artifacts.numenta.org/numenta/nupic/9e17ca7caef03a2b1aa925e010ee76c1a74ec3cc/setuptools-8.0.4-py2.py3-none-any.whl
+
+    localFilePath = os.path.join(wheelDir, wheelName)
+
+    print "Fetching archive from %s..." % url
+    fetchWheel(url, localFilePath)
+
+
+
 def getSha():
   with open(SHA_FILE, "r") as shaFile:
     return shaFile.read().strip()
@@ -49,4 +76,4 @@ def getSha():
 
 if __name__ == "__main__":
   sha = getSha()
-  fetchNupicEggFor(sha)
+  fetchWheels(sha)
