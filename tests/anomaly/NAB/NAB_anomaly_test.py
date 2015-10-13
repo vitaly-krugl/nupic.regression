9c6be0a72729a1b55630ad178c65f8ed559d2f01
Sagan Bolliger <sbolliger@numenta.com>
2015-10-13 14:33:40 -0700

    Added regression test for NAB and json file with expected values

---
+++ tests/anomaly/NAB/NAB_anomaly_test.py
@@ -0,0 +1,36 @@
+# import datetime
+import unittest2 as unittest
+import json
+# import csv
+# from nupic.frameworks.opf.modelfactory import ModelFactory
+# import rec_center_hourly_model_params
+
+
+
+BENCHMARK_PATH = "tests/anomaly/NAB/benchmark_results.json"
+RESULTS_PATH = "../NAB/results/final_results.json"
+EPS = 10e-2
+
+
+class NABAnomalyTest(unittest.TestCase):
+  """Regression tests for NAB on nupic."""
+
+
+
+  def testNABScoresBenchmark(self):
+    """Test that NAB on nupic produces expected scores."""
+
+    with open(BENCHMARK_PATH) as benchmarkFile:
+      benchmarkData = json.load(benchmarkFile)
+
+    with open(RESULTS_PATH) as resultsFile:
+      resultsData = json.load(resultsFile)
+
+    for benchmarkName in benchmarkData["numenta"]:
+      benchmarkValue = benchmarkData["numenta"][benchmarkName]
+      resultValue = resultsData["numenta"][benchmarkName]
+      self.assertAlmostEqual(benchmarkValue, resultValue, delta=EPS)
+
+
+if __name__ == "__main__":
+  unittest.main()
