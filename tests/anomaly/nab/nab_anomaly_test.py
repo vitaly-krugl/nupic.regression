27dac5ed15d4252ba5de5af94a2aa66945ff1fbf
BoltzmannBrain <alexdlavin@gmail.com>
2016-07-21 07:52:34 -0700

    add tests for numentaTM detector

--- tests/anomaly/nab/nab_anomaly_test.py
+++ tests/anomaly/nab/nab_anomaly_test.py
@@ -1,17 +1,21 @@
-import unittest2 as unittest
+
 import json
 import os
+import unittest
 
 
 BENCHMARK_PATH = "tests/anomaly/nab/benchmark_results.json"
-RESULTS_PATH = os.environ['NAB'] + "/results/final_results.json"
+RESULTS_PATH = os.environ["NAB"] + "/results/final_results.json"
 EPS = 10e-2
 
 
 
 class NABAnomalyTest(unittest.TestCase):
-  """Regression tests for NAB on nupic."""
-
+  """ Regression tests for NAB on nupic.
+  Running the NAB detections here times out the regressions tests, so instead we
+  run the tests from from Travis build script, and simply check the results
+  here.
+  """
 
 
   def testNABScoresBenchmark(self):
@@ -26,7 +30,19 @@ class NABAnomalyTest(unittest.TestCase):
     for benchmarkName in benchmarkData["numenta"]:
       benchmarkValue = benchmarkData["numenta"][benchmarkName]
       resultValue = resultsData["numenta"][benchmarkName]
-      self.assertAlmostEqual(benchmarkValue, resultValue, delta=EPS)
+      self.assertAlmostEqual(benchmarkValue, resultValue, delta=EPS, msg=
+          "Numenta detector scores don't check out for the {} profile".
+          format(benchmarkName)
+      )
+
+    for benchmarkName in benchmarkData["numentaTM"]:
+      benchmarkValue = benchmarkData["numentaTM"][benchmarkName]
+      resultValue = resultsData["numentaTM"][benchmarkName]
+      self.assertAlmostEqual(benchmarkValue, resultValue, delta=EPS, msg=
+          "Numenta TM detector scores don't check out for the {} profile".
+          format(benchmarkName)
+      )
+
 
 
 if __name__ == "__main__":
