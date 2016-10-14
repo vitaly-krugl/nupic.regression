3ea5c6f567e3a55dd045956beb552758846465af
Matthew Taylor <matt@numenta.org>
2016-10-14 14:52:17 -0700

    Trying new dependency scripts.

--- ci/bamboo/configure-test-ubuntu.sh
+++ ci/bamboo/configure-test-ubuntu.sh
@@ -28,32 +28,24 @@
 set -o errexit
 set -o xtrace
 
-# Prepare environment
-if [ -z "${USER}" ]; then
-    USER="docker"
-fi
-export USER
-
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NUPIC_ROOT_DIR="$( cd "${MY_DIR}/../.." && pwd )"
 
-echo "Setting up linux dependencies..."
+# echo "Setting up linux dependencies..."
+#
+# # Install OS dependencies, assuming stock ubuntu:latest
+# apt-get update
+# apt-get install -y \
+#     curl \
+#     wget \
+#     git \
+#     python \
+#     python2.7 \
+#     python2.7-dev \
+#     openssl \
+#     libssl-dev \
+#     libffi-dev
 
-# Install OS dependencies, assuming stock ubuntu:latest
-apt-get update
-apt-get install -y \
-    curl \
-    wget \
-    git \
-    python \
-    python2.7 \
-    python2.7-dev \
-    openssl \
-    libssl-dev \
-    libffi-dev
-
-update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-update-alternatives --set python /usr/bin/python2.7
 
 #
 # Install and start mysql (needed for integration and swarming tests)
@@ -74,22 +66,22 @@ DEBIAN_FRONTEND=noninteractive \
 # Start mysql server
 /usr/bin/mysqld_safe &
 
-echo "Installing pip and setuptools..."
-
-# Tool requirements:
-#   Fleshed out PEP-508 support (Dependency Specification)
-_PIP_VER="8.1.2"
-_SETUPTOOLS_VER="25.2.0"
-
-# Download get-pip.py
-curl --silent --show-error --retry 5 -O http://releases.numenta.org/pip/1ebd3cb7a5a3073058d0c9552ab074bd/get-pip.py
-
-python get-pip.py "$@" --ignore-installed \
-  pip==${_PIP_VER} \
-  setuptools==${_SETUPTOOLS_VER} \
-
-python -c 'import pip; print "pip version=", pip.__version__'
-python -c 'import setuptools; print "setuptools version=", setuptools.__version__'
+# echo "Installing pip and setuptools..."
+#
+# # Tool requirements:
+# #   Fleshed out PEP-508 support (Dependency Specification)
+# _PIP_VER="8.1.2"
+# _SETUPTOOLS_VER="25.2.0"
+#
+# # Download get-pip.py
+# curl --silent --show-error --retry 5 -O http://releases.numenta.org/pip/1ebd3cb7a5a3073058d0c9552ab074bd/get-pip.py
+#
+# python get-pip.py "$@" --ignore-installed \
+#   pip==${_PIP_VER} \
+#   setuptools==${_SETUPTOOLS_VER} \
+#
+# python -c 'import pip; print "pip version=", pip.__version__'
+# python -c 'import setuptools; print "setuptools version=", setuptools.__version__'
 
 # Hack to resolve SNIMissingWarning
 pip install urllib3[secure]
